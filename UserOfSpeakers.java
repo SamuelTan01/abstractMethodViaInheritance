e722af31ac6016ec95682cbb625010381ad2bd75
unknown <stan01@stuy.edu>
2019-03-06 21:34:24 -0500

    hw4C

---
+++ UserOfSpeakers.java
@@ -0,0 +1,7 @@
+public class UserOfSpeakers {
+  public static void main(String[] args) {
+    retriever = new Retriever();
+    Animal animal = retriever;
+    System.out.println(retriever.speak());
+  }
+}
